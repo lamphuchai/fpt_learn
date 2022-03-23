b738f6248349aaf1250d72d4c2957f5d18101aea
lamphuchai <lamphuchai38@gmail.com>
2022-03-23 20:49:42 +0700

    up

--- Tmpfff/person_counter.py
+++ Tmpfff/person_counter.py
@@ -5,14 +5,23 @@ import imutils
 import numpy as np
 from centroidtracker import CentroidTracker
 from matplotlib import pyplot as plt
-
-videotestpath = "./video/test_video.mp4"
+from keras.models import load_model
+from keras.applications.mobilenet_v2 import preprocess_input
+from keras.preprocessing.image import img_to_array
+videotestpath = "./video/mask.mp4"
 protopath = "./model/person.prototxt"
 modelpath = "./model/model_person.caffemodel"
 detector = cv2.dnn.readNetFromCaffe(prototxt=protopath, caffeModel=modelpath)
 
 detector.setPreferableTarget(cv2.dnn.DNN_TARGET_CPU)
 
+maskpath = "./model/mask_detector.model"
+proto_txt_path = './model/deploy.prototxt'
+model_path = './model/res10_300x300_ssd_iter_140000.caffemodel'
+
+face_detector = cv2.dnn.readNetFromCaffe(proto_txt_path, model_path)
+
+mask_detector = load_model('./model/mask_detector.model')
 
 CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
         "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
@@ -62,20 +71,51 @@ def non_max_suppression_fast(boxes, overlapThresh):
     except Exception as e:
         print("Exception occurred in non_max_suppression : {}".format(e))
 
-def person_mask(frame,image):
-    cascPath = os.path.dirname(
-    cv2.__file__) + "/data/haarcascade_frontalface_alt2.xml"
-    faceCascade = cv2.CascadeClassifier(cascPath)
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    faces = faceCascade.detectMultiScale(gray,
-                    scaleFactor=1.1,
-                    minNeighbors=5,
-                    minSize=(60, 60),
-                    flags=cv2.CASCADE_SCALE_IMAGE)
-    for (x, y, w, h) in faces:
-        img = image[y:y+h,x:x+w]
-        cv2.rectangle(frame, (x, y), (x + w, y + h),(0, 255, 0), 3)
+def person_mask(frame):
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104, 177, 123))
 
+    face_detector.setInput(blob)
+    detections = face_detector.forward()
+
+    faces = []
+    bbox = []
+    results = []
+
+    for i in range(0, detections.shape[2]):
+        confidence = detections[0, 0, i, 2]
+
+        if confidence > 0.5:
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+            face = np.expand_dims(face, axis=0)
+
+            faces.append(face)
+            bbox.append((startX, startY, endX, endY))
+
+    if len(faces) > 0:
+        results = mask_detector.predict(faces)
+
+    for (face_box, result) in zip(bbox, results):
+        (startX, startY, endX, endY) = face_box
+        (mask, withoutMask) = result
+
+        label = ""
+        if mask > withoutMask:
+            label = "Mask"
+            color = (0, 255, 0)
+        else:
+            label = "No Mask"
+            color = (0, 0, 255)
+
+        cv2.putText(frame, label, (startX, startY-10), cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
 
 def main():
     cap = cv2.VideoCapture(videotestpath)
@@ -97,6 +137,7 @@ def main():
         detector.setInput(blob)
         person_detections = detector.forward()
         rects = []
+        person_mask(frame)
         for i in np.arange(0, person_detections.shape[2]):
             confidence = person_detections[0, 0, i, 2]
             if confidence > 0.5:
@@ -106,9 +147,10 @@ def main():
                     continue
                 person_box = person_detections[0, 0, i, 3:7] * np.array([W, H, W, H])
                 (startX, startY, endX, endY) = person_box.astype("int")
-                face = frame[startY:endY, startX:endX]
-                person_mask(frame,face)
+                # face = frame[startY:endY, startX:endX]
+                # person_mask(frame)
                 rects.append(person_box)
+
 
         boundingboxes = np.array(rects)
         boundingboxes = boundingboxes.astype(int)
